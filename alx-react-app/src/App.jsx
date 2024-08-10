3fdc53f015952ea553aced84cc01e71ef9c2fa0a
Joshua Musau <musaucreative@gmail.com>
2024-08-10 14:13:10 +0300

    added files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,14 +3,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './Header';
+import MainContent from './MainContent'; 
+import Footer from './Footer';
 function App() {
   
   const [count, setCount] = useState(0)
 
   return (
-    
     <>
+    <div>
+    <Header />
+            <MainContent />
+            <Footer />
+    </div>
      <div>
             <WelcomeMessage />
         </div>
