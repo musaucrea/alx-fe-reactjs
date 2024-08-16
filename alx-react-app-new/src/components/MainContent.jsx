484e47ce9e4182e6b20834b6942c8cb3b1aec243
Joshua Musau <musaucreative@gmail.com>
2024-08-16 19:27:14 +0300

    changes to maincontent component

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent(){
     return(
       <main style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
       <p style={{ fontSize: '18px', lineHeight: '1.6' }}>
-        Welcome to the main content area! Here we share details about our favorite cities.
+      I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
     );
