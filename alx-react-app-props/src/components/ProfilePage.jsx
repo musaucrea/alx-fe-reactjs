aca6c39e192bb104b4a0d93af5ff36d4d3066ee7
Joshua Musau <musaucreative@gmail.com>
2024-08-17 12:29:44 +0300

    update to the files

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import UserInfo from './UserInfo';
+import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
+
 
 function App() {
     const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,6 +1,6 @@
 // src/UserDetails.jsx
 import React, { useContext } from 'react';
-import UserContext from '.i./UserContext';
+import UserContext from '../UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
