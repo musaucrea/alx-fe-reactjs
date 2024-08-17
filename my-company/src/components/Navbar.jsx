697b5f3f524ef80edb82882d4f297352b82a2f77
Joshua Musau <musaucreative@gmail.com>
2024-08-17 14:51:58 +0300

    update to navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
+    <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' , display: 'flex', justifyContent: 'center',  }}>
       <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
       <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
+      
     </nav>
   );
 }
