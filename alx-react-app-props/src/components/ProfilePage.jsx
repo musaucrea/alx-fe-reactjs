208021f8b2fa28998c6118ae3879de6da78d4e6b
Joshua Musau <musaucreative@gmail.com>
2024-08-17 13:40:20 +0300

    update to the file

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,14 +1,22 @@
 // ProfilePage.jsx
 import React from 'react';
-import UserDetails from './UserDetails';
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
 
+const UserProfile = () => {
+    const { userData } = useContext(UserContext);
 const ProfilePage = () => {
   return (
-    <div>
-      <UserDetails />
-      {/* Other components */}
-    </div>
+    <><div>
+          <UserDetails />
+          {/* Other components */}
+      </div><div>
+              <h1>{userData.name}</h1>
+              <p>{userData.email}</p>
+              {/* Other user profile details */}
+          </div></>
   );
 };
-
+};
+export default UserProfile;
 export default ProfilePage;
