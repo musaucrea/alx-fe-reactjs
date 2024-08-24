2249dcea1aee47f963929aa00c63978774d72007
Joshua Musau <musaucreative@gmail.com>
2024-08-25 00:46:19 +0300

    update the recipelist

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,34 @@
-import useRecipeStore from './recipeStore';
+// src/components/RecipeList.jsx
+import React, { useEffect } from 'react';
+import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import SearchBar from './SearchBar';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const recipes = useRecipeStore(state => state.filteredRecipes);
+  const filterRecipes = useRecipeStore(state => state.filterRecipes);
+
+  useEffect(() => {
+    filterRecipes();
+  }, [filterRecipes]);
 
   return (
     <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <SearchBar />
+      <div>
+        {recipes.length > 0 ? (
+          recipes.map(recipe => (
+            <div key={recipe.id}>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+              <Link to={`/recipe/${recipe.id}`}>View Details</Link>
+              {/* Add a link to view the recipe details */}
+            </div>
+          ))
+        ) : (
+          <p>No recipes found</p>
+        )}
+      </div>
     </div>
   );
 };
