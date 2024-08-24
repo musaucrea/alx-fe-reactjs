9ac5df96e191fb9a97b862e81608e144781b25dd
Joshua Musau <musaucreative@gmail.com>
2024-08-24 16:42:42 +0300

    update the ccomponents and store files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,8 +1,7 @@
+import { useState } from 'react'
+import React from 'react';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RecipeDetails from './components/RecipeDetails';
-
 
 function App() {
   return (
@@ -10,15 +9,7 @@ function App() {
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
-      <Router>
-    <Routes>
-      <Route path="/" element={<RecipeList />} />
-      <Route path="/recipe/:id" element={<RecipeDetails />} />
-      <Route path="/add" element={<AddRecipeForm />} />
-    </Routes>
-  </Router>
     </div>
-   
   );
 }
 
--- recipe-sharing-app/src/Components/RecipeList.jsx
+++ recipe-sharing-app/src/Components/RecipeList.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
-import useRecipeStore from './recipeStore';
-import { Link } from 'react-router-dom';
-
+import React from 'react';
+import useRecipeStore from '../recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore(state => state.recipes);
 
   return (
     <div>
-      {recipes.map((recipe) => (
+      {recipes.map(recipe => (
         <div key={recipe.id}>
-        <h3><Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link></h3>
+          <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
         </div>
       ))}
