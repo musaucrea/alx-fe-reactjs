5dad4efaccd33835226f913dd1807063e13f0431
Joshua Musau <musaucreative@gmail.com>
2024-08-24 23:54:32 +0300

    update app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,25 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-
+import RecipeDetails from './components/RecipeDetails';
+import EditRecipeForm from './components/EditRecipeForm';
 
 function App() {
   return (
-   
-      <Router>
+    <Router>
       <div className="App">
         <h1>Recipe Sharing App</h1>
-        <AddRecipeForm />
-        <RecipeList />
-        <Switch>
-          <Route path="/recipe/:recipeId" component={RecipeDetails} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+          <Route path="/edit/:recipeId" element={<EditRecipeForm />} />
+        </Routes>
       </div>
     </Router>
   );
 }
 
 export default App;
+
