84e64d8e5af70fd0dbbe802ce4c90bfca15260d5
Joshua Musau <musaucreative@gmail.com>
2024-08-24 19:46:35 +0300

    renamed directory

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import useRecipeStore from './recipeStore';
 
 const AddRecipeForm = () => {
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
