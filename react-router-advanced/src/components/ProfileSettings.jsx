fa600dd2d1e2af18bf746bd03f540e83c703b3ac
builderjo <joshuanguku7@gmail.com>
2024-08-31 12:39:13 +0300

    update to profile.jsx

---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,13 @@
+// src/components/ProfileSettings.jsx
+import React from 'react';
+
+const ProfileSettings = () => {
+  return (
+    <div>
+      <h3>Profile Settings</h3>
+      <p>Adjust your profile settings here...</p>
+    </div>
+  );
+};
+
+export default ProfileSettings;
