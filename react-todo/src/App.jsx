2e29779cfca99e64a63e74a75eefef7ad8e6b515
builderjo <joshuanguku7@gmail.com>
2024-08-31 13:53:45 +0300

    update to app.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.jsx
+import React from 'react';
+import TodoList from './components/TodoList'; // Adjust the path if needed
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todo List</h1>
+      <TodoList />
     </div>
   );
 }
