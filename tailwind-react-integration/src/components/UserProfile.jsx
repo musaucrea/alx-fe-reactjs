2dd3781ebb923f7e29d20971e911d1f9e88a0b85
musaucrea <musaucreative@gmail.com>
2024-09-07 22:47:55 +0300

    added changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto"
+          className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
         />
         <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">
           John Doe
@@ -17,6 +17,7 @@ function UserProfile() {
   }
   
   export default UserProfile;
+  
   
   
   
